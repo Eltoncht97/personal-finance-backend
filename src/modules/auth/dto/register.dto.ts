9a7edb0a27e08e389900907afb7bf5d91bdd0d54
Elton Chavez <elton.mct@gmail.com>
2026-08-27 01:04:42 -0300

    Update register dto

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,16 +1,25 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
   @IsString()
   @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
   name!: string;
 
-  @IsString()
   @IsEmail()
   @IsNotEmpty()
   email!: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(72)
   password!: string;
 }
