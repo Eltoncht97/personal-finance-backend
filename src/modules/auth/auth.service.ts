650cd3025f5456e32f3327f7666887c061a411c1
Elton Chavez <elton.mct@gmail.com>
2026-08-27 21:26:29 -0300

    login in auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { RegisterDto } from './dto/register.dto';
 import { UserRepository } from '../users/repositories/interfaces/user.repository';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -30,6 +35,29 @@ export class AuthService {
       id: user.id,
       name: user.name,
       email: user.email,
+    };
+  }
+
+  async login(dto: LoginDto) {
+    const existingUser = await this.userRepository.findByEmail(dto.email);
+
+    if (!existingUser) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordMatch = await argon2.verify(
+      existingUser.passwordHash,
+      dto.password,
+    );
+
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return {
+      id: existingUser.id,
+      name: existingUser.name,
+      email: existingUser.email,
     };
   }
 }
