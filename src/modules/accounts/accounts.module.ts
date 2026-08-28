f15a7b75b83b54407b983844fbacee845a2dc1a9
Elton Chavez <elton.mct@gmail.com>
2026-08-28 20:26:22 -0300

    adding controller in accounts module

--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AccountRepository } from './repositories/interfaces/account.repository';
 import { PrismaAccountRepository } from './repositories/implementations/prisma-account.repository';
+import { AccountsService } from './accounts.service';
+import { AccountsController } from './accounts.controlle';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [],
+  controllers: [AccountsController],
   providers: [
+    AccountsService,
     { provide: AccountRepository, useClass: PrismaAccountRepository },
   ],
 })
